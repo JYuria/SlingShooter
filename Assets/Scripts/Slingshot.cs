06b3b1e3b73c1edf7ccf1bd06f14057dff0c2730
Julia Weigel <julia.weigel@stud.h-da.de>
2015-04-27 11:50:45 +0200

    Added Script.

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject Launchpoint;
+
+	void Awake(){
+		Transform LaunchpointTrans = transform.Find ("Launchpoint");
+		Launchpoint = LaunchpointTrans.gameObject;
+		Launchpoint.SetActive(false);
+	}
+
+	void OnMouseEnter(){
+		Launchpoint.SetActive(true);
+	}
+
+	void OnMouseExit(){
+		Launchpoint.SetActive(false);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
